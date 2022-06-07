c7ceb8a5ee4caa0ed609e82c4835024b7709a19a
Alexandru Smeu <alexandru.smeu@pss.ro>
2022-06-07 17:13:13 +0300

    Moved `PerformanceMonitor` class in its own header

--- examples/src/12_performance_monitor_class.cpp
+++ examples/src/12_performance_monitor_class.cpp
@@ -1,37 +1,5 @@
 #include "headers.h"
-
-#include <chrono>
-
-class PerfMonitor
-{
-private:
-    using SteadyClock  = std::chrono::steady_clock;
-    using HighResClock = std::chrono::high_resolution_clock;
-    using TimePoint    = std::chrono::time_point<SteadyClock>;
-
-    TimePoint start;
-    TimePoint end;
-
-public:
-    PerfMonitor(): start(), end() { }
-    ~PerfMonitor() { }
-
-    void Start(){
-        start = HighResClock::now();
-    }
-    void End(){
-        end = HighResClock::now();
-    }
-
-    // get the duration - default as milliseconds
-    //      you can get as a different unit by using the type parameter
-    //          pm.Duration<std::micro>()
-    template<class Dur = std::milli, intmax_t _Nx = Dur::num, intmax_t _Dx = Dur::den,
-             std::enable_if_t< std::is_same_v<Dur, std::ratio<_Nx, _Dx>>, bool > = true>
-    auto Duration() {
-        return std::chrono::duration<double, Dur>(end - start).count();
-    }
-};
+#include "PerformanceMonitor.h"
 
 
 void testPerfMonitorClass() {
